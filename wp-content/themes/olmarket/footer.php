7b1bd105a636f9bf21df1b09e6b9564ea28c20a2
test <lilysun@Lilys-MacBook-Pro.local>
2015-02-15 17:13:49 +0000

    add google analystic code

--- wp-content/themes/olmarket/footer.php
+++ wp-content/themes/olmarket/footer.php
@@ -27,5 +27,20 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/slideCarousel.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/main.js"></script>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38576416-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
 </body>
 </html>
