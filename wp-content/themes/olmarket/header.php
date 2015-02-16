bc51d5d54780a7d2268498a1ec45de95a42006c5
rocabee-david <david@rocabee.com>
2015-02-16 19:44:16 +0000

    update the title of the website

--- wp-content/themes/olmarket/header.php
+++ wp-content/themes/olmarket/header.php
@@ -1,8 +1,8 @@
 <!doctype html>
 <html>
 <head>
+   <title>Online Marketing Solution</title>
    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory')?>/style.css">
-   
 </head>
 <body>
   
