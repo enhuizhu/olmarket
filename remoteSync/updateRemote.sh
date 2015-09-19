18cf0494c325104d1b7f097c3c63990346544a0f
rocabee-david <david@rocabee.com>
2015-09-19 15:17:59 +0100

    update mysql database

--- remoteSync/updateRemote.sh
+++ remoteSync/updateRemote.sh
@@ -7,7 +7,7 @@ mysqldump -u"$LOCAL_USER" -p"$LOCAL_PASSWORD" "$DATABASE" > "$MYSQL_FILE"
 #after generating mysql file should put everything to responsitory
 cd ..
 git add .
-git commit -m "update"
+git commit -m "update mysql database"
 git push -u origin HEAD
 
 
